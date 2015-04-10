58e4800a180e506afb82784a8e0655a1b14989b1
Luis Diaz <ldifernan@gmail.com>
2015-04-10 09:20:23 +0200

    some changes

--- src/main/scala/com/stratio/model/spark/FlightDsl.scala
+++ src/main/scala/com/stratio/model/spark/FlightDsl.scala
@@ -2,7 +2,10 @@ package com.stratio.model.spark
 
 import scala.language.implicitConversions
 import com.stratio.model.Flight
+import com.stratio.utils.ParserUtils
 import org.apache.spark.rdd.RDD
+import org.apache.spark.SparkContext._
+
 
 class FlightCsvReader(self: RDD[String]) {
 
@@ -37,15 +40,15 @@ class FlightCsvReader(self: RDD[String]) {
       if (errorList.isEmpty) {
         Seq(Right(Flight(fields)))
       }
-      else{
-        errorList.map(error =>{
+      else {
+        errorList.map(error => {
           Left((error, line))
         })
       }
     }
     )
   }
-
+}
 
   class FlightFunctions(self: RDD[Flight]) {
 
@@ -54,7 +57,12 @@ class FlightCsvReader(self: RDD[String]) {
      * Obtain the minimum fuel's consumption using a external RDD with the fuel price by Month
      *
      */
-    def minFuelConsumptionByMonthAndAirport(fuelPrice: RDD[String]): RDD[(String, Short)] = ???
+    def minFuelConsumptionByMonthAndAirport(fuelPrice: RDD[String]): RDD[(String, Short)] = {
+      val origMonthDist = self.map(linea=>((linea.date.monthOfYear() , linea.origin),linea.distance)).reduceByKey(_+_)
+        .map(linea=> ((linea._1._1),(linea._1._2,linea._2, fuelPrice)))
+      origMonthDist.map(x=>(x._1,(x._2._1,x._2._2 * x._2._3.toInt))
+      result.map(x=>(x._3._1, (x._1,x._2,x._3._2))).reduceByKey((x,y)=>if(x._3 < y._3) x else y)
+    }
 
     /**
      *
@@ -62,17 +70,23 @@ class FlightCsvReader(self: RDD[String]) {
      *
      */
     def averageDistanceByAirport: RDD[(String, Float)] = {
-
+      val distances = self.map(linea => (linea.origin,linea.distance.toFloat))
+      distances.reduceByKey(_+_).map(linea=>(linea._1,linea._2/self.count()))
     }
+
   }
 
 
-trait FlightDsl {
 
-  implicit def flightParser(lines: RDD[String]): FlightCsvReader = new FlightCsvReader(lines)
 
-  implicit def flightFunctions(flights: RDD[Flight]): FlightFunctions = new FlightFunctions(flights)
-}
+  trait FlightDsl {
 
-object FlightDsl extends FlightDsl
+    implicit def flightParser(lines: RDD[String]): FlightCsvReader = new FlightCsvReader(lines)
+
+    implicit def flightFunctions(flights: RDD[Flight]): FlightFunctions = new FlightFunctions(flights)
+  }
+
+  object FlightDsl extends FlightDsl
+
+
 
